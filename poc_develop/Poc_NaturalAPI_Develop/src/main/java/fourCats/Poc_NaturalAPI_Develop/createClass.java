c397385555485ee33aa4e4990add315b6be59d0b
matteoinfantino <matteoinfa@gmail.com>
2020-02-25 13:52:00 +0100

    aggiunta possibilità di inserire il tipo di ritorno dei metodi + corretto formato file .java

--- poc_develop/Poc_NaturalAPI_Develop/src/main/java/fourCats/Poc_NaturalAPI_Develop/createClass.java
+++ poc_develop/Poc_NaturalAPI_Develop/src/main/java/fourCats/Poc_NaturalAPI_Develop/createClass.java
@@ -3,7 +3,9 @@ package fourCats.Poc_NaturalAPI_Develop;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class createClass {
 	private String name;
@@ -12,7 +14,7 @@ public class createClass {
 	
 	createClass(){
 		String[] names = json.getScenario().split(" ");
-		names[0] = names[0].substring(0,1).toLowerCase() + names[0].substring(1);
+		names[0] = names[0].substring(0,1).toUpperCase() + names[0].substring(1);
 		for (int i=1; i<names.length; i++) {
 			names[i] = names[i].substring(0,1).toUpperCase() + names[i].substring(1);
 			name = names[i-1] + names[i];
@@ -21,15 +23,19 @@ public class createClass {
 	}
 	
 	public void create() {
-		File file = new File("test.class");
+		File file = new File("test.java");
+		Scanner scanner = new Scanner(System.in);
 		try {
 			FileWriter fileWriter = new FileWriter(file);
 			fileWriter.write("public class " + name + " {\n\n");
 			for(String method : methods) {
-				fileWriter.write("	public void " + method + "() {\n\n	}\n");
+				System.out.println("Inserisci il tipo di ritorno del metodo " + method);
+				fileWriter.write("	public " + scanner.next() + " "+ method + "() {\n\n	}\n");
 			}
 			fileWriter.write("\n}");
 			fileWriter.close();
+			scanner.close();
+			System.out.println("Classe java creata!");
 		}
 		catch(IOException ioe) {
 			System.out.println("error");
---
+++ poc_develop/Poc_NaturalAPI_Develop/test.java
@@ -0,0 +1,19 @@
+public class WithdrawCash {
+
+	public int withdraw_cash() {
+
+	}
+	public void enter_pin() {
+
+	}
+	public void enter_amount() {
+
+	}
+	public bool check_pin() {
+
+	}
+	public int give_cash() {
+
+	}
+
+}
