20e4a909583ea66153062dba7be9f76f9fb6f169
Matteo <matteoinfa@gmail.com>
2020-03-11 18:54:38 +0100

    eliminata libreria json.simple dal develop, sostituita con Jackson

--- poc_develop/Poc_NaturalAPI_Develop/src/main/java/fourCats/Poc_NaturalAPI_Develop/ObtainInfoBAL.java
+++ poc_develop/Poc_NaturalAPI_Develop/src/main/java/fourCats/Poc_NaturalAPI_Develop/ObtainInfoBAL.java
@@ -1,16 +1,13 @@
 package fourCats.Poc_NaturalAPI_Develop;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObtainInfoBAL {
 	
@@ -18,31 +15,28 @@ public class ObtainInfoBAL {
 	private Map<String,ArrayList<String>> operationParametersMap;
 	
 	ObtainInfoBAL(){
-		JSONParser parser = new JSONParser();
-	    
-        Object obj;
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		File file = new File("bal.json");
+
 		try {
-			obj = parser.parse(new FileReader("bal.json"));
-	        JSONObject jsonObject =  (JSONObject) obj;	        
-	        
-	        JSONArray actors = (JSONArray) jsonObject.get("actors");
-	        
+			JsonNode parent = objectMapper.readValue(file, JsonNode.class);
+			
+			JsonNode actors = parent.get("actors");
+			
 	        operationsMap = new HashMap<String,String>();
 	        operationParametersMap = new HashMap<String,ArrayList<String>>();
-	        
-	        for(Object actor : actors) {
-	        	JSONObject jsonActor = (JSONObject) actor;
-	        	
-	        	JSONArray operations = (JSONArray) jsonActor.get("operations");
-	        	
-	        	for(Object op : operations) {
-		        	JSONObject jsonOp = (JSONObject) op;
-		        	String operation = (String) jsonOp.get("name").toString();
-	        		String type = (String) jsonOp.get("type");
-	        		if(type == null) {
-	        			type = "void";
-	        		}
-		        	
+			
+			for(JsonNode actor : actors) {
+				JsonNode operations = actor.get("operations");
+				
+				for(JsonNode op : operations) {
+					String operation = op.get("name").asText();
+					String type = op.get("type").asText();
+					if(type == null) {
+						type = "void";
+					}
 		        	//camelCase metodi
 		        	String[] split = operation.split("_");
 		        	operation = "";
@@ -55,27 +49,18 @@ public class ObtainInfoBAL {
 		        	
 		        	operationsMap.put(operation,type);
 		        	ArrayList<String> parameters = new ArrayList<String>();
-		        	JSONArray par = (JSONArray) jsonOp.get("parameters");
-		        	for(Object parameter : par) {
-		        		JSONObject jsonParameter = (JSONObject) parameter;
-		        		String name = (String) jsonParameter.get("name");
-		        		String typePar = (String) jsonParameter.get("type");
+		        	JsonNode par = op.get("parameters");
+		        	for(JsonNode parameter : par) {
+		        		String name = parameter.get("name").asText();
+		        		String typePar = parameter.get("type").asText();
 		        		parameters.add(typePar + " " + name);
 		        	}
 		        	operationParametersMap.put(operation, parameters);
-		        }
-	        }
-
-	        
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				}
+			}
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();
 		}
 	}
 	
